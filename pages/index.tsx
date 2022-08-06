888243a2d2114527659147cad20ea4b07e387c29
jmurphy5613 <jmurphy5613@gmail.com>
2022-08-05 21:57:19 -0700

    addedp preliminary landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-
+      <h1 className={styles.title}>Welcome to Stupid Shit</h1>
+      <button className={styles["play-button"]}>Play</button>
     </div>
   )
 }
